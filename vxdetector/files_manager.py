2469e144a71d8d8f8685cef64134e45ff1b90360
Stefan Janssen <stefan.m.janssen@gmail.com>
2022-07-05 12:11:33 +0200

    codestyle

--- vxdetector/files_manager.py
+++ vxdetector/files_manager.py
@@ -4,16 +4,19 @@ import os
 import tempfile
 import shutil
 
-def tmp_dir(path, temp_path): #erstellt tmp verzeichnis; sollte es auch deleten 
+
+def tmp_dir(path, temp_path):
+    """erstellt tmp verzeichnis; sollte es auch deleten"""
     if os.path.exists(temp_path):
         shutil.rmtree(temp_path)
     else:
-        temp_path = tempfile.mkdtemp(suffix=None, prefix='tmp_files_', dir=path) + '/'
-        
-        
+        temp_path = tempfile.mkdtemp(suffix=None, prefix='tmp_files_',
+                                     dir=path) + '/'
     return temp_path
 
-def get_lib(): #findet die richtigen ordner/dateien, damit das programm funktioniert
+
+def get_lib():
+    """findet die richtigen ordner/dateien, damit das programm funktioniert"""
     programm_path = os.path.dirname(__file__) + '/'
     if os.path.exists(programm_path+'Output/'):
         pass
@@ -23,17 +26,19 @@ def get_lib(): #findet die richtigen ordner/dateien, damit das programm funktion
         if os.path.exists(programm_path+'Indexed_bt2/annoted_ref.bed'):
             pass
         else:
-            print('ERROR: It seems the 16S variable region boundary reference file is missing.')
+            print('ERROR: It seems the 16S variable region boundary reference '
+                  'file is missing.')
         if os.path.exists(programm_path+'Indexed_bt2/85_otus.fasta'):
             pass
         else:
-            print('ERROR: It seems the Greengenes "85_otus.fasta" file is missing.')
+            print('ERROR: It seems the Greengenes "85_otus.fasta" file is'
+                  ' missing.')
         if os.path.exists(programm_path+'Indexed_bt2/85_otus_aligned.fasta'):
             pass
         else:
-            print('ERROR: It seems the Greengenes "85_otus_aligned.fasta" file is missing.')
+            print('ERROR: It seems the Greengenes "85_otus_aligned.fasta" '
+                  'file is missing.')
     else:
         print('ERROR: It seems the "Indexed_bt2" directory is missing.')
-        
+
     return programm_path
-    
