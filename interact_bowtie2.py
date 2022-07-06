2ea3c9d3f2e76364906d07050e1a69a702a80e02
Johannes Groos <jgroos@algbi-stud-pc04.desktop>
2022-07-06 11:11:57 +0200

    temporaere sam file erstellung in kommentare gepackt

--- interact_bowtie2.py
+++ interact_bowtie2.py
@@ -32,7 +32,7 @@ def mapbowtie2(fasta_file, read2_file, path, temp_path, mode, file_type):
     return aligned_path
 
     
-'''
+''' 
 def mapbowtie2(fasta_file, read2_file, path, temp_path, mode, file_name, dir_name, dir_path, file_type):
     file_name, dir_name, dir_path = Output_counter.directory_navi(file_name, path, dir_name, dir_path)
     SAM_path = f'{dir_path}{file_name}.sam'
@@ -44,11 +44,11 @@ def mapbowtie2(fasta_file, read2_file, path, temp_path, mode, file_name, dir_nam
     bed_logpath = f'{temp_path}bed.log'
     if mode == 'unpaired':
         aligned_path = f'{temp_path}unpaired.bam'
-        cmd = f'{bowtie2_path} -x {index_path} {file_type} -U {fasta_file} --fast 2> {log_path} -S {SAM_path} '
+        cmd = f'{bowtie2_path} -x {index_path} {file_type} -U {fasta_file} --fast -S {SAM_path} 2> {log_path}'
         cmd2 = f'{samtools_path} view -b -S -F 4 {SAM_path} -o {aligned_path}'
     else:
         aligned_path = f'{temp_path}paired.bed'
-        cmd = f'{bowtie2_path} -x {index_path} -1 {fasta_file} -2 {read2_file} --fast 2> {log_path} -S {SAM_path}'
+        cmd = f'{bowtie2_path} -x {index_path} -1 {fasta_file} -2 {read2_file} --fast -S {SAM_path} 2> {log_path}'
         cmd2 = f'{samtools_path} view -b -S -F 4  {SAM_path} | {bedtools_path} bamtobed -bedpe -i stdin > {aligned_path} 2> {bed_logpath}'
     os.system(cmd)
     os.system(cmd2)
