c1941ed9f24b59c223f2c2bc2daede8f47852b88
Stefan Janssen <stefan.m.janssen@gmail.com>
2024-11-15 13:01:20 +0100

    avoid reading all lines into memory, to keep footprint low

--- vxdetector/VXdetector.py
+++ vxdetector/VXdetector.py
@@ -13,38 +13,44 @@ from interact_bedtools import overlap
 import random
 import gzip
 
+
 def sample_fastq(file_path, sample_size, sampled_indices=None):
     '''Get random parts from the FASTQ file based on shared indices for paired-end reads.'''
-    sampled_reads = []  # List to hold sampled reads
-    open_func = gzip.open if file_path.endswith('.gz') else open
 
-    # Count total reads in the file by iterating line by line
-    with open_func(file_path, 'rt') as f:
-        total_reads = sum(1 for _ in f) // 4
+    def _count_generator(reader):
+        b = reader(1024 * 1024)
+        while b:
+            yield b
+            b = reader(1024 * 1024)
 
-    # Adjust sample_size if it's greater than total_reads
-    if sample_size > total_reads:
-        sample_size = total_reads
+    def _get_filehandle(file_path):
+        # open file either plain or as gzip compressed file
+        FH = None
+        if file_path.endswith('.gz'):
+            FH = gzip.open(file_path, 'rb')
+        else:
+            FH = open(file_path, 'rb')
+        return FH
 
-    if sampled_indices is None:
-        sampled_indices = sorted(random.sample(range(total_reads), sample_size))
+    # first pass: count number of lines in the file
+    FH = _get_filehandle(file_path)
+    c_generator = _count_generator(FH.read)
+    number_lines = sum(buffer.count(b'\n') for buffer in c_generator)
+    assert number_lines % 4 == 0, "file %s does not contain a multiple of 4 lines! %i" % (file_path, number_lines)
+    FH.close()
 
-    with open_func(file_path, 'rt') as f:
-        read_idx = 0
-        read_buffer = []
-        for line in f:
-            read_buffer.append(line)
-            if len(read_buffer) == 4:  # Once we have a full read
-                if read_idx in sampled_indices:
-                    sampled_reads.extend(read_buffer)  # Add the read if it matches the sampled index
-                read_buffer = []  # Clear the buffer for the next read
-                read_idx += 1  # Move to the next read
+    # second pass: iterate through file with line counts
+    # add next 4 lines to sampled_reads, iff linecount is in random selection
+    sampled_readlines = []  # List to hold sampled reads
+    sel_reads = random.sample(range(int(number_lines / 4) + 1), sample_size)
+    FH = _get_filehandle(file_path)
+    ln = 0
+    for ln, line in enumerate(FH):
+        if (ln // 4) in sel_reads:
+            sampled_readlines.append(str(line, encoding='utf-8'))
+    FH.close()
 
-                # Stop if we've collected enough reads
-                if len(sampled_reads) >= sample_size * 4:
-                    break
-
-    return sampled_reads, sampled_indices
+    return sampled_readlines, sel_reads
 
 
 def save_sampled_fastq(sampled_reads, output_path):
@@ -82,14 +88,14 @@ def do_output(result, new_file, single_file):
     # Convert columns to numeric where applicable
     for column in result:
         result[column] = pd.to_numeric(result[column], errors='ignore')
-    
+
     if single_file is False:
         result = do_statistic(result)  # Add statistics if processing multiple files
     else:
         result = result[['Number of Reads', 'Unaligned Reads [%]', 'Not properly paired',
                          'Sequenced variable region', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6',
                          'V7', 'V8', 'V9', 'Not aligned to a variable region']]
-    
+
     # Write the result DataFrame to a CSV file
     result.to_csv(new_file, index=True)
 
@@ -105,16 +111,17 @@ def workflow(file_dir, new_file, write_csv, sample_size, bowtie2_params):
     # Check if the input path exists
     if not os.path.exists(file_dir):
         raise ValueError(f"The provided path {file_dir} does not exist.")
-    
+
     # Check if there are FASTQ files in the directory
     if glob.glob(f'{file_dir}**/*.fastq*', recursive=True) == [] and os.path.isdir(file_dir):
         files_manager.tmp_dir(path, temp_path)
         raise ValueError('There were no FASTQ files in this directory')
-    
+
     # Process single file
     if os.path.isfile(file_dir):
         single_file = True
         file_name = os.path.basename(file_dir)
+
         read2_file = os.path.join(os.path.dirname(file_dir), file_name.replace('_R1_', '_R2_'))  # Expected paired file
 
         # Check if paired-end data is available
@@ -144,7 +151,7 @@ def workflow(file_dir, new_file, write_csv, sample_size, bowtie2_params):
         if paired is True and Output_counter.rawincount(f'{temp_path}paired.bed') == 0:
             files_manager.tmp_dir(path, temp_path)
             raise ValueError('This file has no Reads of the required mapping-quality')
-        
+
         # Perform overlap analysis using Bedtools
         overlap(path, temp_path, aligned_path)
         if not paired and Output_counter.rawincount(f'{temp_path}BED.bed') == 0:
@@ -155,7 +162,7 @@ def workflow(file_dir, new_file, write_csv, sample_size, bowtie2_params):
         file_name = file_name.rsplit('.f', 1)[0]
         file_name = file_name.replace('_R1_001', '')
         result[file_name] = Output_counter.create_row(temp_path, paired)
-        
+
     # Process directory of files
     elif os.path.isdir(file_dir):
         single_file = False
@@ -166,7 +173,7 @@ def workflow(file_dir, new_file, write_csv, sample_size, bowtie2_params):
             paired = False
             if '_R2_' in fq_file:
                 continue  # Skip R2 files to avoid processing duplicates
-            
+
             file_name = os.path.basename(fq_file)
             read2_file = os.path.join(os.path.dirname(fq_file), file_name.replace('_R1_', '_R2_'))
             if '_R1_' in file_name and os.path.exists(read2_file):
@@ -175,7 +182,7 @@ def workflow(file_dir, new_file, write_csv, sample_size, bowtie2_params):
             # Sample reads from R1 (and R2 if paired) with same indices
             sampled_reads_R1, sampled_indices = sample_fastq(fq_file, sample_size)
             sampled_reads_R2 = sample_fastq(read2_file, sample_size, sampled_indices)[0] if paired else None
-            
+
             # Save sampled reads to temporary files
             temp_fastq_R1 = os.path.join(temp_path, 'sampled_R1.fastq')
             save_sampled_fastq(sampled_reads_R1, temp_fastq_R1)
@@ -195,10 +202,10 @@ def workflow(file_dir, new_file, write_csv, sample_size, bowtie2_params):
             file_name = file_name.rsplit('.f', 1)[0]
             file_name = file_name.replace('_R1_001', '')
             result[file_name] = Output_counter.create_row(temp_path, paired)
-                
+
     # Clean up temporary directory
     files_manager.tmp_dir(path, temp_path)
-    
+
     # Output the result as CSV or display
     do_output(result, new_file, single_file)
     if write_csv is True:
@@ -211,17 +218,17 @@ def main():
     parser = argparse.ArgumentParser(prog='VX detector', description=(
         'This program tries to find which variable region of the 16S sequence was sequenced'))
     parser.add_argument('dir_path', help=('Directory path of the directory containing multiple fastq or fasta files.'))
-    parser.add_argument('-o', '--output', dest='output_file', default=sys.stdout, 
+    parser.add_argument('-o', '--output', dest='output_file', default=sys.stdout,
                         help='User can specify a file format in which the output is written in the Output folder.')
-    parser.add_argument('-c', '--csv', dest='write_csv', action='store_true', 
+    parser.add_argument('-c', '--csv', dest='write_csv', action='store_true',
                         help='If set the output will be written in a .csv file in the Output folder')
-    parser.add_argument('-s', '--sample_size', dest='sample_size', type=int, default=1000, 
+    parser.add_argument('-s', '--sample_size', dest='sample_size', type=int, default=1000,
                         help='Number of reads to sample from each FASTQ file')
     parser.add_argument('-b','--bowtie2_params', dest='bowtie2_params', default='--threads 4, --fast',
                         help='Additional parameters to pass to Bowtie2')
     # Parse the user input arguments
     args = parser.parse_args()
-    
+
     # Start the workflow with provided arguments
     workflow(args.dir_path, args.output_file, args.write_csv, args.sample_size, args.bowtie2_params)
 
